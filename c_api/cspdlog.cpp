78b330d99e97ca04642594e1f4908b5f9a060085
Chanilino <chan@cube>
2016-03-03 23:57:24 +0100

    Added more funcions and test to c api

--- c_api/cspdlog.cpp
+++ c_api/cspdlog.cpp
@@ -38,24 +38,43 @@ extern "C"
 		va_end (args);
 		if(msg){
 	        	c->logger->info() << msg;
+			free(msg);
 		}
 	}
 
 	void clog_debug(clogger* c, const char * std, ... ){
-        	c->logger->debug("This is another message with custom format");
+		va_list args;
+		char * msg;
+	  	va_start (args, std);
+		clog_write(&msg, c->logger, spdlog::level::info,  std, args);
+		va_end (args);
+		if(msg){
+	        	c->logger->debug() << msg;
+			free(msg);
+		}
 	}
 
 	void cspd_set_level(clevel_enum level){
-		throw  std::logic_error("Not implemented");
+		assert(TRACE == (clevel_enum)spdlog::level::trace);
+		assert(DEBUG == (clevel_enum)spdlog::level::debug);
+		assert(INFO == (clevel_enum)spdlog::level::info);
+		assert(NOTICE == (clevel_enum)spdlog::level::notice);
+		assert(WARN == (clevel_enum)spdlog::level::warn);
+		assert(ERR == (clevel_enum)spdlog::level::err);
+		assert(CRITICAL ==(clevel_enum)spdlog::level::critical);
+		assert(ALERT == (clevel_enum)spdlog::level::alert);
+		assert(EMERG == (clevel_enum)spdlog::level::emerg);
+		assert(OFF == (clevel_enum)spdlog::level::off);
+		spdlog::set_level((spdlog::level::level_enum)level);
+	}
+	void cspd_set_pattern(const char* pattern){
+		spdlog::set_pattern(pattern);
 	}
 
 	clogger* cspd_rotating_logger_mt(const char* logger_name, const char* filename, size_t max_file_size, size_t max_files, bool force_flush ){
 		throw  std::logic_error("Not implemented");
 	}
 
-	void cspd_set_pattern(const char* pattern){
-		throw  std::logic_error("Not implemented");
-	}
 
 	void cspd_set_async_mode(size_t queue_size, const casync_overflow_policy overflow_policy){
 		throw  std::logic_error("Not implemented");
--- test_c_api/testcspdlog.c
+++ test_c_api/testcspdlog.c
@@ -1,8 +1,12 @@
 #include "cspdlog.h"
 
 int main(void){
+	int i;
 	clogger* sout = create_stdout_logger_mt("stdout");
 	clog_info(sout, "Hola %s", "WORLD");
+        cspd_set_pattern("*** [%H:%M:%S %z] [thread %t] %v ***");
+        for (i = 0; i < 10; ++i)
+		clog_info(sout, "Hola %s: %d", "WORLD", i);
         cspdlog_drop_all();
 	return 0;
 }
