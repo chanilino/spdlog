89eefa63c42fb3fc4a1739fe56c4a5b79e907c5c
Chanilino <chan@cube>
2016-03-07 19:56:52 +0100

    added basic test

--- test_c_api/spdlog-bench.c
+++ test_c_api/spdlog-bench.c
@@ -11,10 +11,10 @@ int main(int argc, char*argv[])
     int howmany = 1000000;
     int i;
     ///Create a file rotating logger with 5mb size max and 3 rotated files
-    clogger* logger =  create_simple_file_logger_st("file_logger", "logs/spd-bench-st.txt", false);
-    c_logger_set_pattern(logger, "[%Y-%b-%d %T.%e]: %v");
+    clogger* logger =  cspd_simple_file_logger_st("file_logger", "logs/spd-bench-st.txt", false);
+    cspd_logger_set_pattern(logger, "[%Y-%b-%d %T.%e]: %v");
     for( i  = 0 ; i < howmany; ++i)
-	clog_info(logger, "spdlog message #%d: This is some text for your pleasure", i);
+	cspd_info(logger, "spdlog message #%d: This is some text for your pleasure", i);
 
     return 0;
 }
--- test_c_api/testcspdlog.c
+++ test_c_api/testcspdlog.c
@@ -2,11 +2,11 @@
 
 int main(void){
 	int i;
-	clogger* sout = create_stdout_logger_mt("stdout");
-	clog_info(sout, "Hola %s", "WORLD");
+	clogger* sout = cspd_stdout_logger_mt("stdout");
+	cspd_info(sout, "Hola %s", "WORLD");
         cspd_set_pattern("*** [%H:%M:%S %z] [thread %t] %v ***");
         for (i = 0; i < 10; ++i)
-		clog_info(sout, "Hola %s: %d", "WORLD", i);
+		cspd_info(sout, "Hola %s: %d", "WORLD", i);
         cspd_drop_all();
 	return 0;
 }
